236f067c49a3af94aba6b0e75a27fbea770dda15
EC2 Default User <ec2-user@ip-172-31-6-39.us-east-2.compute.internal>
2021-10-11 23:15:44 +0000

    restart left-looking

--- left-looking-basic/main.cc
+++ left-looking-basic/main.cc
@@ -34,6 +34,7 @@ double** ludecompose(const double* m, const int n) {
     
     solvedUpper[0] = fromOrig;
     double upperPivotSum = (j == 0) ? 0 : l[n] * solvedUpper[j*n];
+    cout << "upper PIVOT SUM START = " << upperPivotSum << endl;
     if (j == 1){
       upperPivotSum += l[n] * solvedUpper[0];
     }
@@ -49,13 +50,19 @@ double** ludecompose(const double* m, const int n) {
   
         const double thisSolution = m[i * n + j] - triangularSum;
         u[i * n + j] = thisSolution;
-        cout << "i: " << i << "\tj: " << j << endl;
+        // cout << "i: " << i << "\tj: " << j << endl;
         solvedUpper[i] = thisSolution;
   
         // Perform the addition/product for step 2 as we compute the necessary
         // elements. Use it while it's in register?
+        // cout << "L[" << j + 1 << ", " << i+1 -1<< "] = " << l[j * n + i-1] << endl;
         upperPivotSum += l[j * n + i] * thisSolution;
+        cout << "upper PIVOT SUM += " << l[j * n + i] * thisSolution << endl;
       }
+      // cout << "jn = " << j*n << endl;
+      if (j != n-1)upperPivotSum += l[(j)*n + 1] * solvedUpper[j-1]; 
+      cout << "UPPER PIVOT SUM = " << upperPivotSum<<endl;
+      
     }
 
     double u22 = 0;
@@ -66,6 +73,8 @@ double** ludecompose(const double* m, const int n) {
     // Step 2: Solve the single diagonal element of U22
     if (j != 0) {
       u22 = m[j * n + j] - upperPivotSum;
+      cout << "A = " << m[j*n+j] << endl;
+      cout << "U22 = " << u22 <<endl << endl;
       u[j * n + j] = u22;
     }
     solvedUpper[j] = u22;
@@ -74,8 +83,9 @@ double** ludecompose(const double* m, const int n) {
     sumLowerRows[j] += 1;
     for (int i = j+1; i < n; i++) {
       double thisSolution = 0;
-      // if(j == 1 && i==2)
-        // cout <<"test"<<endl<<endl<<endl<<endl<<endl << "u22: " << u22<<endl;;
+      if(j == 2 && i==3){
+        cout <<"test"<<endl<<endl<<endl<<endl<<endl << "u22: " << u22<<endl;;
+      }
       double sumL31U12Product = 0;
       if (j != 0) {
          double u12 = 0;
@@ -93,12 +103,12 @@ double** ludecompose(const double* m, const int n) {
         } else {
           l31 = sumLowerRows[i];
         }
-        // cout << "Solved Upper [" << i-j-1 << "] = " << u12 << endl;
-        // cout << "l31 = " << l31 << endl;
+        cout << "Solved Upper [" << i-j-1 << "] = " << u12 << endl;
+        cout << "l31 = " << l31 << endl;
         sumL31U12Product = l31 * u12;//(i - 1) * n + k
-        // cout << "sum L31U12 Product = " << sumL31U12Product <<endl;
-        // cout << "SumLowerRows[" << i <<"] = " << sumLowerRows[i] <<endl;
-        // cout << "A[" << i-j+2 << ", " << j+1 << "] = " << m[(i - j+1) * n + j] << endl;
+        cout << "sum L31U12 Product = " << sumL31U12Product <<endl;
+        cout << "SumLowerRows[" << i <<"] = " << sumLowerRows[i] <<endl;
+        cout << "A[" << i-j+2 << ", " << j+1 << "] = " << m[(i - j+1) * n + j] << endl;
         thisSolution = (m[(i - j+1) * n + j] - sumL31U12Product) * invU22;
       } else {
         thisSolution = (m[(i - j) * n + j] - sumL31U12Product) * invU22;
